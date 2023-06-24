f71891ccd99b42b2201111e976b61133fe07ee84
pjys211 <fly_98@naver.com>
2023-06-24 16:03:15 +0900

    스프링부트 테스트로 전환하기

--- src/main/java/com/example/produceorderservice/product/ProductService.java
+++ src/main/java/com/example/produceorderservice/product/ProductService.java
@@ -1,5 +1,9 @@
 package com.example.produceorderservice.product;
 
+import org.springframework.stereotype.Component;
+
+
+@Component
 class ProductService {
     private final ProductPort productPort;
 
